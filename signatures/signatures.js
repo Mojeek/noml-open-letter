db46c3a0491d2981e3b210b2de35fa7a77754c1d
Joshua Long <josh@mojeek.com>
2023-11-02 13:56:09 +0000

    Add signatures, Fixes #18, Fixes #19, Fixes #20

--- signatures/signatures.js
+++ signatures/signatures.js
@@ -2,37 +2,49 @@ let individuals = [{
 	"name": "Joshua Long ",
 	"url": "https://mstdn.social/@privacydingus",
 	"affiliation": "Mojeek",
-}, {
+},	{
 	"name": "Colin Hayhurst",
 	"url": "https://twitter.com/ColinHayhurst",
 	"affiliation": "Mojeek",
-}, {
+},	{
 	"name": "Andreas Schamanek",
 	"affiliation": "Individual",
-}, {
+},	{
 	"name": "Nikolaos Dimitropoulos",
 	"affiliation": "Individual",
-}, {
+},	{
 	"name": "Richard Lees",
 	"affiliation": "Mojeek",
-}, {
+},	{
 	"name": "Jack Yan, CEO",
 	"url": "https://jyanet.com",
 	"affiliation": "Jack Yan & Associates",
-}, {
+},	{
 	"name": "Niklas Poslovski",
 	"url": "https://codeberg.org/nipos",
 	"affiliation": "Individual",
-}, {
+},	{
 	"name": "mirabilos",
 	"url": "http://www.mirbsd.org/",
 	"affiliation": "Individual",
-}, {
+},	{
 	"name": "Bobby Hiltz",
 	"affiliation": "Individual",
-}, {
+},	{
 	"name": "James North",
 	"url": "http://www.jamesnorth.net/",
 	"affiliation": "Individual",
+},	{
+	"name": "Phil Höfer",
+	"url": "https://suma-ev.social/@phoefer",
+	"affiliation": "SUMA-EV/MetaGer",
+},	{
+	"name": "Peter Beackley",
+	"url": "https://playfultechnology.co.uk",
+	"affiliation": "Playful Technology Limited",
+},	{
+	"name": "Joe Devon",
+	"url": "https://www.linkedin.com/in/joedevon",
+	"affiliation": "Yuda Digital",
 }
 ]
